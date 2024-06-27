8c79e9daa6449c99b8caac9b2bddc2964152c7d7
shpylovyi1007 <yevheniishpylovyi398@gmail.com>
2024-06-27 14:41:29 +0300

    .

---
+++ src/components/Allert/MyAllert.jsx
@@ -0,0 +1,17 @@
+import clsx from "clsx";
+import css from "./AlLert.module.css";
+
+const Alert = ({ variant, outlined, elevated, children }) => {
+  return (
+    <p
+      className={clsx(css[variant], {
+        [css.isOutlined]: outlined,
+        [css.isElevated]: elevated,
+      })}
+    >
+      {children}
+    </p>
+  );
+};
+
+export default Alert;
---
+++ src/components/App/App.jsx
@@ -0,0 +1,37 @@
+import Product from "../Product/Product";
+import Alert from "../Allert/MyAllert";
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <h1>Best selling</h1>
+
+        <Product
+          name="Tacos With Lime"
+          imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+          price={10.99}
+        />
+        <Product
+          name="Fries and Burger"
+          imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+          price={14.29}
+        />
+      </div>
+      <div>
+        <Alert variant="info">
+          Would you like to browse our recommended products?
+        </Alert>
+        <Alert variant="error">
+          There was an error during your last transaction
+        </Alert>
+        <Alert variant="success">
+          Payment received, thank you for your purchase
+        </Alert>
+        <Alert variant="warning">
+          Please update your profile contact information
+        </Alert>
+      </div>
+    </>
+  );
+}
